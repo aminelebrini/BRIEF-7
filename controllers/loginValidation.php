540aba9aa14e07a96bf0da706068e93e6e114085
aminelebrini <aminelebrini4@gmail.com>
2025-12-17 12:44:05 +0100

    il y a des errors dans le session

--- controllers/loginValidation.php
+++ controllers/loginValidation.php
@@ -1,16 +1,35 @@
 <?php
+    session_start();
     include __DIR__ . "/../data/connect_db.php";
-
-    if($_SERVER['REQUEST_METHOD'] === 'POST')
+    if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['signin']))
     {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+        $email = $_POST['emaillog'];
+        $passwordlogi = $_POST['passwordlog'];
 
         $query = "SELECT * FROM users WHERE email = '$email'";
-        $resul = mysqli_query($connect, $query);
+        $declar = mysqli_prepare($connect, $query);
+        mysqli_stmt_bind_param($declar, "s", $email);
+        mysqli_stmt_execute($declar);
+        $resul = mysqli_stmt_get_result($declar);
         $user = mysqli_fetch_assoc($resul);
 
-        print_r($user);
+        
+        if(password_verify($passwordlogi, $user['password']))
+        {
+            $_SESSION['user'] = [
+                'id' => $user['id'],
+                'fullname' => $user['fullname'],
+                'email' => $user['email'],
+                'url' => $user['url']
+            ];
+
+            if(isset($_SESSION))
+            {
+                header("Location: /profile");
+                exit;
+            }
+        }
+        
     }
 ?>
 
