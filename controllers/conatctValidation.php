87e5f20f59a1054f79268a6c87c41871da069642
aminelebrini <aminelebrini4@gmail.com>
2025-12-18 22:10:34 +0100

    j'ai fini le travaille sur la page de contact et la creation du tableau de contacts pour recevoir les message

--- controllers/conatctValidation.php
+++ controllers/conatctValidation.php
@@ -1,4 +1,6 @@
 <?php
+
+    include __DIR__ . "/../data/connect_db.php";
     $Contact = [];
     $messageErr = "";
     $verifMessage = "";
@@ -39,25 +41,24 @@
             {
                 $messageErr = '<div class="popup text-red-700 ">Veuillez entrer un message valide.</div>';
             }
-                        
+            
 
         if (empty($nameErr) && empty($emailErr) && empty($messageErr)) {
 
-        echo '<div class="popupsucess bg-green-500 p-5 fixed top-20 right-0">
-            <h1 class="text-2xl text-white"><i class="fas fa-check-square"></i> Votre message a bien été envoyé !</h1>
-        </div>';
-        echo '
-            <form id="autoSend" action="https://formspree.io/f/xgvgzled" method="POST">
-                <input type="hidden" name="name" value="' . $verifName . '">
-                <input type="hidden" name="email" value="' . $verifEmail . '">
-                <input type="hidden" name="message" value="' . $verifMessage . '">
-            </form>
-            <script>
-                document.getElementById("autoSend").submit();
-            </script>';
-        }
-        echo '<div class="popupsucess bg-red-500 p-5 fixed top-20 right-0">
-                <h1 class="text-2xl text-white"><i class="fas fa-multiply text-2xl"></i> Votre message n&apos;a pas été envoyé. Veuillez vérifier votre saisie. !</h1>
+            $query = "INSERT INTO contacts (ffullname, email, message) VALUES (?, ?, ?)";
+            $stmt = mysqli_prepare($connect, $query);
+            mysqli_stmt_bind_param($stmt, "sss", $verifName, $verifEmail, $verifMessage);
+
+            if (mysqli_stmt_execute($stmt)) {
+                echo '<div class="popupsucess bg-green-500 p-5 fixed top-20 right-0">
+                    <h1 class="text-2xl text-white">Votre message a bien été envoyé !</h1>
+                </div>';
+            }
+        } else {
+            echo '<div class="popupsucess bg-red-500 p-5 fixed top-20 right-0">
+                <h1 class="text-2xl text-white">Votre message n\'a pas été envoyé. Veuillez vérifier votre saisie.</h1>
             </div>';
+        }
+
     }
 ?>
